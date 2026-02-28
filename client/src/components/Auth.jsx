596b1655fb3c8e44c5b4c6c6e5f88d6b034ad642
joelinrani123 <joelinrani44@gmail.com>
2026-02-28 11:38:03 +0530

    Updated production backend URL

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://chatty-server-p80i.onrender.com'
         //url
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
